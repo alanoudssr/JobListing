9685bb503c8811d914f0d147b932ce34c4f6355a
alanoudssr <alanoudssr@gmail.com>
2021-01-31 19:32:22 +0300

    fix routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import { BrowserRouter as Router } from "react-router-dom";
 import './shared/general.css';
 import AppRoutes from './AppRoutes';
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <AppRoutes />
-    </Router>
+    <AppRoutes />
   );
 }
 
