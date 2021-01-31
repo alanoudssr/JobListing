8ec1be06074990b0ed678166158ce3bc483868d0
alanoudssr <alanoudssr@gmail.com>
2021-01-31 18:00:22 +0300

    fix routing

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import List from "./screens/List"
 import Show from "./screens/Show"
 import ErrorPage from "./screens/ErrorPage"
@@ -8,17 +8,15 @@ import Help from "./screens/Help"
 const AppRoutes = () => {
 
     return (
-        <Router>
-            <Switch>
-                <Route exact path="/" component={List} />
-                <Route path="/show/:title" component={Show} />
-                <Route path="/help" component={Help} />
-                <Route path="/404" component={ErrorPage} />
-                <Route path="*"
-                    component={() => <Redirect to={'/404'} />}
-                />
-            </Switch>
-        </Router>
+        <Switch>
+            <Route exact path="/" component={List} />
+            <Route path="/show/:title" component={Show} />
+            <Route path="/help" component={Help} />
+            <Route path="/404" component={ErrorPage} />
+            <Route path="*"
+                component={() => <Redirect to={'/404'} />}
+            />
+        </Switch>
     )
 
 }
