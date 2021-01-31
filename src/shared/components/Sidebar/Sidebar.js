2a1413a3bbe1ca62224e19d1a695a2fcbcb9c107
alanoudssr <alanoudssr@gmail.com>
2021-01-31 19:35:47 +0300

    fix help redirect

--- src/shared/components/Sidebar/Sidebar.js
+++ src/shared/components/Sidebar/Sidebar.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react'
 import './sidebar.css';
 import logo from "../../../assets/imgs/logo.png"
 import { HiChatAlt2 } from 'react-icons/hi';
+import { Link } from 'react-router-dom'
 
 const Sidebar = ({ active }) => {
     return (
@@ -22,7 +23,7 @@ const Sidebar = ({ active }) => {
                 <div className='t-flex-row t-flex__nowrap t-flex-between'>
                     <HiChatAlt2 className='t-icon-lg mt-m1 mr-2' />
                     <p className='t-sm pl-1'>
-                        if you have any questions while filling the form in, welcome to our <a className='t-link' href="/help">Help Center</a>
+                        if you have any questions while filling the form in, welcome to our <Link className='t-link' to="/help">Help Center</Link>
                     </p>
                 </div>
             </div>
