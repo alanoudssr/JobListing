5d08cfb563b037a03da5e27c44ee22b2f97f3641
alanoudssr <alanoudssr@gmail.com>
2021-01-31 17:24:34 +0300

    fix routing

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <HashRouter>
+  <HashRouter basename='/'>
     <App />
   </HashRouter>,
   document.getElementById('root')
